df8937b01b944532d9d3ee26a08a899889866399
Aaminah <aaminah.halipoto@example.com>
2022-10-20 10:32:48 -0500

    updated bootstrap integration

--- env/app.py
+++ env/app.py
@@ -2,24 +2,56 @@
 import firebase_admin
 import pyrebase
 import json
-from firebase_admin import credentials, auth
-from flask import Flask, request, render_template
+from firebase_admin import credentials, auth, db
+from flask import Flask, request, render_template, redirect
+from forms import AddUserForm, RemoveUserForm
 from functools import wraps
-
+from twilio.rest import Client
 import collections
 from collections.abc import MutableMapping
 
 #App configuration
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'any secret string'
+
 #Connect to firebase
 cred = credentials.Certificate('schannel-538d4-firebase-adminsdk-fp41y-f2a6a4646d.json')
-firebase = firebase_admin.initialize_app(cred)
+firebase = firebase_admin.initialize_app(cred, {
+	'databaseURL': 'https://schannel-538d4-default-rtdb.firebaseio.com/'	
+})
 pb = pyrebase.initialize_app(json.load(open('sChannelconfig.json')))
 
-#Data source
+#Initialize messaging information
+account_sid = 'AC91c5afebf10d075eb56a120c957845ee' 
+auth_token = '6b6b5ed94e20a62803d7d704952c996a'
+client = Client(account_sid, auth_token) 
+
+#Creating variables from DB
+ref = db.reference('SChannel')
+channels = ref.child('Channels')
+channelgroups = ref.child('ChannelGroups')
+workflow = ref.child('Workflows')
+
+channelName = ref.child("Tables").child("Channels").child("Name").get()
+channelEmail = ref.child("Tables").child("Channels").child("Email").get()
+channelPhone = ref.child("Tables").child("Channels").child("Phone Number").get()
+channelItems = ref.child("Tables").child("Channels").child("Items Bought").get()
+
+channelGroupID = ref.child("Tables").child("Channel Groups").child("GroupID").get()
+channelGroupEmail = ref.child("Tables").child("Channel Groups").child("Email Addresses").get()
+channelGroupWhatsapp = ref.child("Tables").child("Channel Groups").child("Whatsapp Numbers").get()
+channelGroupSMS = ref.child("Tables").child("Channel Groups").child("SMS Numbers").get()
+
+workflowID = ref.child("Tables").child("Workflow").child("ID").get()
+workflowEmailTemp = ref.child("Tables").child("Workflow").child("Email template").get()
+workflowWhatsappTemp = ref.child("Tables").child("Workflow").child("Whatsapp template").get()
+workflowSMSTemp = ref.child("Tables").child("Workflow").child("SMS template").get()
+
+#Additional test data source
 users = [{'uid': 1, 'name': 'Aaminah Halipoto'}]
 
+#Function to authorize user token
 def check_token(f):
     @wraps(f)
     def wrap(*args,**kwargs):
@@ -35,7 +67,7 @@ def check_token(f):
 
 #Api route to get users
 @app.route('/api/userinfo')
-@check_token
+#@check_token
 def userinfo():
     return {'data': users}, 200
 
@@ -67,9 +99,31 @@ def token():
     except:
         return {'message': 'There was an error logging in'},400
 
-@app.route('/api/adduser')
+#Api route to test fetching from firebase data
+@app.route('/api/attempt')
+def attempt():
+    info=channelEmail
+    return render_template('attempt.html', info=info), 200
+
+#Api route to basic, untouched bootstrap modal
+@app.route('/api/addusercopy')
+def addusercopy():
+    return render_template('userinfocopy.html'), 200
+
+#Api route to bootstrap modal newly integrated to html
+@app.route('/api/adduser', methods=['GET', 'POST'])
 def adduser():
     return render_template('userinfo.html')
+
+#Api route to bootstrap modal with integrated python forms
+@app.route('/api/action', methods=['GET', 'POST'])
+def register():
+    adduser_form = AddUserForm()
+    removeuser_form = RemoveUserForm()
+    return render_template(
+        'action.html', 
+        adduser_form=adduser_form, 
+        removeuser_form=removeuser_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- env/forms.py
+++ env/forms.py
@@ -1,5 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length
 
-#class RegistrationForm(FlaskForm):
-    
+class AddUserForm(FlaskForm):
+    first_name = StringField('First Name',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    last_name = StringField('Last Name',
+                        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired()])
+    phone_number = StringField('Phone Number',
+                                     validators=[DataRequired()])
+    submit = SubmitField('Submit user')
+
+class RemoveUserForm(FlaskForm):
+    first_name = StringField('First Name',
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    last_name = StringField('Last Name',
+                        validators=[DataRequired(), Length(min=2, max=20)])
+    submit = SubmitField('Remove user')
